f765f1e1c2c603105b74124fe82de52854f2abbe
paperspace <travis.h.oneill@gmail.com>
2020-11-16 15:04:49 -0500

    get server running on paperspace

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
-// #include <iostream>
+#include <iostream>
 // #include "libhyperpixel/utils.h"
 #include "libhyperpixel/server.h"
 // #include "cpp-httplib-0.7.14/httplib.h"
 
 int main() {
-  // std::cout << utils::get_frame_index_label(43, 8200) << std::endl;
-  server::runserver("localhost", 8000);
-  // bool out = utils::create_project("example");
-  // std::cout << out << std::endl;
-  // test2
+  int port = 8080;
+  std::cout << "Listening on port " <<  port << std::endl;
+  server::runserver("0.0.0.0", port);
+  std::cout << "Error server stopped" << std::endl;
   return 0;
 }
